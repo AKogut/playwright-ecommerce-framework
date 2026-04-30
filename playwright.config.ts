6ecc0a5705813d633085942fc0df6f4fc6341f1e
Andrii Kohut <a.kogut01@gmail.com>
2026-04-30 11:28:20 +0200

    chore: add environment config (issue#8) (#66)
    Co-authored-by: Andrii Kohut <kohut@AndriisacStudio.lan>

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig, devices } from '@playwright/test';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import { frameworkConfig } from './src/config/environment';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   testDir: './tests',
+  timeout: frameworkConfig.testTimeoutMs,
+  globalTimeout: frameworkConfig.globalTimeoutMs,
+  expect: {
+    timeout: frameworkConfig.expectTimeoutMs,
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -25,11 +23,10 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    baseURL: frameworkConfig.baseUrl,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
